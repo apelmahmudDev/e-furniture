e8c27c8388f7759500bcf49800a51fa58daac782
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-05-10 11:29:30 +0600

    ✅wishlist

--- src/views/components/common/Wishlist/index.tsx
+++ src/views/components/common/Wishlist/index.tsx
@@ -1,16 +1,43 @@
 import NotFound from "../NotFound";
-import { Box } from "@mui/material";
 import HeartBrokenOutlinedIcon from "@mui/icons-material/HeartBrokenOutlined";
+import { Box, Button, Avatar, Typography, IconButton } from "@mui/material";
+import { IMAGES } from "../../../../constants/themeData";
+import { useStyles } from "./styled";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 
 const Wishlist = () => {
+	const classes = useStyles();
+
 	return (
 		<Box>
 			{/* not found */}
-			<Box my={4} textAlign="center">
-				<NotFound
-					message="No products in the wishlist."
-					icon={<HeartBrokenOutlinedIcon />}
-				/>
+			{false && (
+				<Box my={4} textAlign="center">
+					<NotFound
+						message="No products in the wishlist."
+						icon={<HeartBrokenOutlinedIcon />}
+					/>
+				</Box>
+			)}
+			{/* wishlist product*/}
+			<Box>
+				{[...Array(3)].map((item, idx) => (
+					<Box key={idx} className={classes.wishlist}>
+						<Avatar src={IMAGES.HeroOneImg} alt="product" />
+						<Box>
+							<Typography variant="subtitle1">CH24 Wishbone Chair</Typography>
+							<Typography color="primary" variant="subtitle2">
+								$452.00 <span className={classes.date}>Date: 21 jun 2021</span>
+							</Typography>
+						</Box>
+						<IconButton size="small">
+							<AddShoppingCartOutlinedIcon fontSize="small" />
+						</IconButton>
+					</Box>
+				))}
+				<Button sx={{ my: 1 }} fullWidth variant="outlined">
+					View Cart
+				</Button>
 			</Box>
 		</Box>
 	);
--- src/views/components/common/Wishlist/styled.ts
+++ src/views/components/common/Wishlist/styled.ts
@@ -1,3 +1,23 @@
 import { makeStyles } from "@mui/styles";
 
-export const useStyles = makeStyles({});
+export const useStyles = makeStyles((theme) => ({
+	wishlist: {
+		marginBottom: 16,
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-between",
+		gap: 8,
+
+		"& .MuiAvatar-root": {
+			width: "65px !important",
+			height: "65px !important",
+			borderRadius: "0 !important",
+		},
+	},
+	date: {
+		fontSize: 12,
+		marginLeft: 8,
+		fontWeight: 400,
+		color: theme.palette.text.secondary,
+	},
+}));
