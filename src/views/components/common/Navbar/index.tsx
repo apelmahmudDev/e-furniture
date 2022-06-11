f5bd86a084461e9e11d1c5e00d46c86a3148e1ee
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-06-11 17:59:53 +0600

    ✅update link

--- src/views/components/common/Navbar/index.tsx
+++ src/views/components/common/Navbar/index.tsx
@@ -58,23 +58,23 @@ const Navbar = (props: any) => {
 					<Container>
 						<Toolbar disableGutters>
 							<ChairIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-							<Typography
-								variant="h6"
-								noWrap
-								component="a"
-								href="/"
-								sx={{
-									mr: 2,
-									display: { xs: "none", md: "flex" },
-									fontFamily: "Titillium Web",
-									fontWeight: 700,
-									letterSpacing: ".1rem",
-									color: "inherit",
-									textDecoration: "none",
-								}}
-							>
-								e-furniture
-							</Typography>
+							<Link to={ROUTING_TREE.HOME}>
+								<Typography
+									variant="h6"
+									noWrap
+									sx={{
+										mr: 2,
+										display: { xs: "none", md: "flex" },
+										fontFamily: "Titillium Web",
+										fontWeight: 700,
+										letterSpacing: ".1rem",
+										color: "inherit",
+										textDecoration: "none",
+									}}
+								>
+									e-furniture
+								</Typography>
+							</Link>
 
 							<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
 								<IconButton
