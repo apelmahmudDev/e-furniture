e835cb73ae6b5135a73da778cfd36621d5369452
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-06-11 16:42:46 +0600

    :white_check_mark: update button progress

--- src/views/components/common/ButtonProgress/index.tsx
+++ src/views/components/common/ButtonProgress/index.tsx
@@ -5,33 +5,33 @@ import Button from "@mui/material/Button";
 
 const ButtonProgress = ({
 	btnType,
+	variantType,
 	btnText,
 	isLoading,
 }: {
 	btnType?: "button" | "submit" | "reset" | undefined;
+	variantType?: "text" | "contained" | "outlined" | undefined;
 	btnText: string;
 	isLoading: boolean;
 }) => {
 	return (
-		<Box sx={{ display: "flex", alignItems: "center" }}>
-			<Box sx={{ position: "relative" }}>
-				<Button type={btnType} variant="text" disabled={isLoading}>
-					{btnText}
-				</Button>
-				{isLoading && (
-					<CircularProgress
-						size={24}
-						sx={{
-							color: green[500],
-							position: "absolute",
-							top: "50%",
-							left: "50%",
-							marginTop: "-12px",
-							marginLeft: "-12px",
-						}}
-					/>
-				)}
-			</Box>
+		<Box sx={{ position: "relative" }}>
+			<Button type={btnType} disabled={isLoading} variant={variantType}>
+				{btnText}
+			</Button>
+			{isLoading && (
+				<CircularProgress
+					size={24}
+					sx={{
+						color: green[500],
+						position: "absolute",
+						top: "50%",
+						left: "50%",
+						marginTop: "-12px",
+						marginLeft: "-12px",
+					}}
+				/>
+			)}
 		</Box>
 	);
 };
