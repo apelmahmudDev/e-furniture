8d17b30b1c4590590ba641cfbcc63614d5196de9
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-06-12 20:51:16 +0600

    ✅add icons

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -4,8 +4,28 @@ import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutl
 import PercentIcon from "@mui/icons-material/Percent";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MenuIcon from "@mui/icons-material/Menu";
+import DashboardIcon from "@mui/icons-material/GridViewRounded";
+import MallIcon from "@mui/icons-material/LocalMallRounded";
+import InventoryIcon from "@mui/icons-material/Inventory2";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import SettingsIcon from "@mui/icons-material/Settings";
+import PowerIcon from "@mui/icons-material/PowerSettingsNew";
+import PersonIcon from "@mui/icons-material/Person";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 export {
+	ShoppingCartOutlinedIcon,
+	FavoriteBorderOutlinedIcon,
+	PersonIcon,
+	PowerIcon,
+	SettingsIcon,
+	AutorenewIcon,
+	InventoryIcon,
+	MallIcon,
+	DashboardIcon,
+	MenuIcon,
 	ChairIcon,
 	HeartBrokenOutlinedIcon,
 	AddShoppingCartOutlinedIcon,
