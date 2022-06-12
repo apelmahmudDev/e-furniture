1a5ef9e419ac572c00cfa8d41be5735b4304fc51
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-06-12 07:55:21 +0600

    fixed navigation route link

--- src/views/components/common/Navbar/index.tsx
+++ src/views/components/common/Navbar/index.tsx
@@ -113,24 +113,24 @@ const Navbar = (props: any) => {
 								</Menu>
 							</Box>
 							<ChairIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-							<Typography
-								variant="h5"
-								noWrap
-								component="a"
-								href=""
-								sx={{
-									mr: 2,
-									display: { xs: "flex", md: "none" },
-									flexGrow: 1,
-									fontFamily: "monospace",
-									fontWeight: 700,
-									letterSpacing: ".1rem",
-									color: "inherit",
-									textDecoration: "none",
-								}}
-							>
-								e-furniture
-							</Typography>
+							<Link to={ROUTING_TREE.HOME}>
+								<Typography
+									variant="h5"
+									noWrap
+									sx={{
+										mr: 2,
+										display: { xs: "flex", md: "none" },
+										flexGrow: 1,
+										fontFamily: "monospace",
+										fontWeight: 700,
+										letterSpacing: ".1rem",
+										color: "inherit",
+										textDecoration: "none",
+									}}
+								>
+									e-furniture
+								</Typography>
+							</Link>
 							<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
 								{pages.map((page) => (
 									<Button
